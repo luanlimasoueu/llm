611811e0280029611605b814e44fada2466a5b60
okluan <luanemerson8@gmail.com>
2024-05-18 23:37:15 -0300

    Alterações

---
+++ Bedrock/3_exemplo/chatbot_backend.py
@@ -0,0 +1,7 @@
+import os
+from langchain.llms.bedrock import Bedrock
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConstitutionalChain
+
+#def demo_chatbot():
+
