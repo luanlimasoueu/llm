e47b131d3e1c0fc0a3af93679ec42ed54981bbda
okluan <luanemerson8@gmail.com>
2024-05-19 13:05:44 -0300

    Alterações

--- Bedrock/3_exemplo/chatbot_backend.py
+++ Bedrock/3_exemplo/chatbot_backend.py
@@ -12,34 +12,19 @@ from langchain.chains import ConstitutionalChain
 import os
 from dotenv import load_dotenv
 
-# Carregando as variáveis de ambiente do arquivo .env
 load_dotenv()
-# Acessando a variável de ambiente API_KEY
 
-boto3.setup_default_session(aws_access_key_id = os.getenv("aws_access_key_id"),
-                            aws_secret_access_key= os.getenv("aws_secret_access_key"),
-                            region_name = os.getenv("region_name"))
-bedrock = boto3.client(service_name="bedrock-runtime")
-
-bedrock = boto3.client(service_name="bedrock-runtime")
-
-prompt = "Camberra is capital of Australia"
-
-body = json.dumps({
-    "inputText": prompt
-})
-
-model_id = 'amazon.titan-embed-text-v1'
-accept = 'application/json'
-content_type = 'application/json'
-
-response = bedrock.invoke_model(
-    body = body,
-    modelId = model_id,
-    accept = accept,
-    contentType = content_type
-)
-
-response_body = json.loads(response['body'].read())
-embedding = response_body.get('embedding')
-print(embedding)
+def demo_chatbot(input_text):
+    demo_llm = Bedrock(
+    model_id='meta.llama2-70b-chat-v1',
+    aws_access_key_id = os.getenv("aws_access_key_id"),
+    aws_secret_access_key= os.getenv("aws_secret_access_key"),
+    region_name = os.getenv("region_name"),
+    model_kwargs={
+        "temperature": 0.9,
+        "top_p": 0.5,
+        "max_gen_len": 512
+    })
+    return  demo_llm.predict(input_text)
+response = demo_chatbot("Hi, what is yor name?")
+print(response)
