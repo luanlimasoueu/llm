1a4c579d36504381e3d0f49f9daed0926e3ad86a
okluan <luanemerson8@gmail.com>
2024-05-19 14:51:39 -0300

    Alterações

--- Bedrock/4_exemplo/chatbot_backend.py
+++ Bedrock/4_exemplo/chatbot_backend.py
@@ -1,9 +1,9 @@
-import os
 import boto3
 import json
+import os
 from langchain.llms.bedrock import Bedrock
 from langchain.memory import ConversationBufferMemory
-from langchain.chains import ConstitutionalChain
+from langchain.chains import  ConversationChain
 
 
 from dotenv import load_dotenv
@@ -12,7 +12,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-def demo_chatbot(input_text):
+def demo_chatbot():
     
     boto3.setup_default_session(
     aws_access_key_id = os.getenv("aws_access_key_id"),
@@ -32,6 +32,19 @@ def demo_chatbot(input_text):
         "max_gen_len": 512
     })
 
-    return  demo_llm.predict(input_text)
-response = demo_chatbot("Hi, what is yor name?")
-print(response)
+    return  demo_llm
+
+def demo_memory():
+    llm_data = demo_chatbot()
+    memory = ConversationBufferMemory(
+        llm = llm_data,
+        max_token_limit = 512
+    )
+    return memory
+
+def demo_conversation (input_text, memory):
+    llm_chain_data = demo_chatbot()
+    llm_conversation = ConversationChain(llm=llm_chain_data, memory=memory, verbose=True)
+
+    chat_reply = llm_conversation.predict(input=input_text)
+    return chat_reply
